dcdc7c341605704d36954ca6193fb0ac0748418b
BaBaSaMa <julong170501@gmail.com>
2020-11-09 20:32:59 +0800

    updated L04

--- C273/C273_L04ToDoList/js/exercise3.js
+++ C273/C273_L04ToDoList/js/exercise3.js
@@ -2,29 +2,53 @@ $(document).ready(function() {
   $("form").submit(function() {
     var message = "";
     if (!$("#idName").val()) {
-      message += "- Name is required. \n";
+      message += "- Name is required.<br>";
     }
-    if(!$("#selCountry").val()) {
-      message += "- Country is required. \n";
+    if (!$("#selCountry").val()) {
+      message += "- Country is required.<br>";
     }
-    if(!$("input[type='radio']:checked").val()) {
-      message += "- Gender is required.\n";
+    if(!$("#idPostal").val()) {
+      message += "- Postal code is required.<br>";
     }
-    if(!$("input[type='checkbox']:checked")) {
-      message += "- At least one color is required.\n";
+    if (!$("input[type='radio']:checked").val()) {
+      message += "- Gender is required.<br>";
     }
-    if(!$("#idPhone").val()) {
-      message += "- Phone is required.\n";
+    if (!$("input[type='checkbox']:checked")) {
+      message += "- At least one color is required.<br>";
     }
-    if(!$("#idEmail").val()) {
-      message += "- Email is required.\n";
+    if (!$("#idPhone").val()) {
+      message += "- Phone is required.<br>";
+    } else {
+      console.log("test1");
+      let regex = new RegExp("[89]\d{7}");
+      if(!regex.test($("#idPhone").val())) {
+        console.log("test2");
+        message += "- Phone number must be starting with 8 or 9 and containing a total of 8 digits.<br>";
+      }
     }
-    if(!$("#idPwd").val()) {
-      message += "- Password is required.\n";
+    if (!$("#idEmail").val()) {
+      message += "- Email is required.<br>";
+    } else {
+      console.log("test3");
+      let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test($("#idEmail").val())) {
+        console.log("Test4");
+        message += "- Email must be entered correctly.<br>"
+      }
     }
-    console.log(message);
-    $("#alertBox").text(message);
+    if (!$("#idPwd").val()) {
+      message += "- Password is required.<br>";
+    } else {
+      let regex = new RegExp("/.{6,8}/g");
+      if(!regex.test($(this).val())) {
+        message += "- Password must be 6 to 8 characters long.<br>";
+      }
+    }
+    if($("#idVerifyPwd").val() != $("#idPwd").val()) {
+      message += "- Password must be the same.<br>"
+    }
+
+    $("#alertBox").html(message);
     return false;
-  
   });
 });
