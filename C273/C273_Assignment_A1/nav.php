9d2b62e2578a426fef406493a94e42633de2c272
Ju Long <19013345@myrp.edu.sg>
2020-12-02 21:27:26 +0800

    commit

--- C273/C273_Assignment_A1/nav.php
+++ C273/C273_Assignment_A1/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light">
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
   <img id="logo" class="navbar-brand" src="./img/skh_logo.png" onclick="directToHome()"/>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
@@ -9,29 +9,51 @@
     <ul class="navbar-nav ml-auto">
       <?php if (isset($_SESSION['user'])) {?>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="#">Home<i class="fas fa-home"></i></a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="#">Logout</a>
+        <a class="nav-link" href="#">Meal Entry<i class="fas fa-edit"></i></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Data Display<i class="fas fa-database"></i></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Logout<i class="fas fa-sign-out-alt"></i></a>
       </li>
 
     <?php } else {?>
       <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#">Login<i class="fas fa-sign-in-alt"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sign up<i class="fas fa-user-plus"></i></a>
       </li>
     <?php }?>
     </ul>
 
   </div>
 </nav>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel='stylesheet' href="css/all.min.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 <script src="https://unpkg.com/@popperjs/core@2"></script>
 <script src="https://unpkg.com/tippy.js@6"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <style media="screen">
+  li {
+    font-size: 20;
+    margin-right: 10px;
+  }
+  #navbarSupportedContent {
+    padding-right: 50px;
+  }
+  i {
+    margin-left: 7px;
+  }
   #logo {
     width: 11%;
     cursor: pointer;
@@ -42,6 +64,11 @@
 
 <script type="text/javascript">
   function directToHome() {
-    location.replace("index.php")
+    location.replace("index.php");
   }
+  $("a").mouseover(function() {
+    $(this).addClass("text-info")
+  }).mouseout(function() {
+    $(this).removeClass("text-info");
+  });
 </script>
