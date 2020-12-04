8af310f9f146c3098a99107e9305ab479cec9836
Ju Long <19013345@myrp.edu.sg>
2020-12-04 17:43:19 +0800

    commit

---
+++ C273/C273_Assignment_A1/login+signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <<?php include './nav.php'?>
+    <div class="container">
+
+      
+
+      <form id="loginForm" action="index.html" method="post">
+        <div class="form-group">
+          <label for="id_username">Please Enter Your Username: </label>
+          <input type="text" class="form-control" id="id_username" name="username"/>
+        </div>
+
+        <div class="form-group">
+          <label for="id_password">Please Enter Your Password: </label>
+          <input type="password" class="form-control" id="id_password" name="username"/>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
