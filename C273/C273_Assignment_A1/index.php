8bfc0ee14e8dff2839011c098a178da3a0d1cbb2
Ju Long <19013345@myrp.edu.sg>
2020-12-10 19:46:11 +0800

    commit

--- C273/C273_Assignment_A1/index.php
+++ C273/C273_Assignment_A1/index.php
@@ -7,7 +7,82 @@ session_start();
 <head>
   <meta charset="utf-8">
   <title></title>
-  <link rel="stylesheet" href="./css/index.css">
+  <style media="screen">
+  .mySlides {
+    display: none
+  }
+
+  #slideshow-img {
+    vertical-align: center;
+    max-width: 90%;
+    padding-top: 5%;
+    padding-left: 10%;
+  }
+
+  .slideshow-container {
+    max-width: 1000px;
+    position: relative;
+    margin: auto;
+  }
+
+  .prev, .next {
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    width: auto;
+    padding: 16px;
+    margin-top: -22px;
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+    transition: 0.6s ease;
+    border-radius: 0 3px 3px 0;
+    user-select: none;
+  }
+
+  .next {
+    right: 0;
+    border-radius: 3px 0 0 3px;
+  }
+
+  .prev:hover, .next:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+
+  .numbertext {
+    color: #f2f2f2;
+    font-size: 12px;
+    padding: 8px 12px;
+    position: absolute;
+    top: 0;
+  }
+
+  @media only screen and (max-width: 300px) {
+    .prev, .next, .text {
+      font-size: 11px
+    }
+  }
+
+  #pages {
+    flex-direction: column;
+    background-color: #E86F4A;
+    height: auto;
+  }
+
+  #pages-section {
+    border-bottom: 1px solid white;
+    flex: 1;
+    padding: 5px;
+    color: white;
+    height: 50px;
+  }
+
+  .icons {
+    height: 25px;
+    width: auto;
+    margin-right: 5px;
+  }
+  </style>
 </head>
 
 <body style="background-image: './img/skgh.png'">
@@ -37,34 +112,34 @@ session_start();
 
     <ul class="col-4" id="pages">
       <li id="pages-section">
-        <a class="nav-link text-white" href="https://www.skh.com.sg/">Visit Our Offical Website</a>
+        <a class="nav-link text-white" href="https://www.skh.com.sg/"><img src="./icons/website.png" class="icons">Visit Our Offical Website</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care/visiting-specialist/Pages/make-or-change-appointment.aspx">Book an Appointment</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care/visiting-specialist/Pages/make-or-change-appointment.aspx"><img src="./icons/booking.png" class="icons">Book an Appointment</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care/conditions-treatments#abdomen">Find a Condition or Treatments</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care/conditions-treatments#abdomen"><img src="./icons/search.png" class="icons">Find a Condition or Treatments</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="https://www.singhealth.com.sg/PatientCare/health-buddy/Pages/Home.aspx">Download Buddy Health App</a>
+        <a class="nav-link text-white" href="https://www.singhealth.com.sg/PatientCare/health-buddy/Pages/Home.aspx"><img src="./icons/appstore.png" class="icons">Download Buddy Health App</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/about-us/corporate-profile">About Us</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/about-us/corporate-profile"><img src="./icons/about.png" class="icons">About Us</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care">Patient Care</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/patient-care"><img src="./icons/patient.png" class="icons">Patient Care</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="https://www.singhealthdukenus.com.sg">Research & Innovation</a>
+        <a class="nav-link text-white" href="https://www.singhealthdukenus.com.sg"><img src="./icons/microscope.png" class="icons">Research & Innovation</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/education-training">Education & Training</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/education-training"><img src="./icons/training.png" class="icons">Education & Training</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/careers">Careers</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/careers"><img src="./icons/career.png" class="icons">Careers</a>
       </li>
       <li id="pages-section">
-        <a class="nav-link text-white" href="http://www.skh.com.sg/giving">Giving</a>
+        <a class="nav-link text-white" href="http://www.skh.com.sg/giving"><img src="./icons/charity.png" class="icons">Giving</a>
       </li>
     </ul>
 
