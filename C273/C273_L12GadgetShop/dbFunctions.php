4fd33f648180ca9db9c63c300681e414b5f1f45e
Ju Long <19013345@myrp.edu.sg>
2021-02-01 23:25:06 +0800

    completed ps

--- C273/C273_L12GadgetShop/dbFunctions.php
+++ C273/C273_L12GadgetShop/dbFunctions.php
@@ -5,7 +5,7 @@
     $db       = "c273_p12";
 
 
-$host = "34.126.76.15";
+$host = "localhost";
 $link = mysqli_connect($host,$username,$password,$db) or
         die(mysqli_connect_error());
 
--- C273/C273_L12GadgetShop/manageGadgetShop.php
+++ C273/C273_L12GadgetShop/manageGadgetShop.php
@@ -19,11 +19,43 @@ mysqli_close($link);
         <link href="css/all.min.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="https://www.paypal.com/sdk/js?client-id=ARumW6pj-y1u7dfjBer-TVXty40io3aZRxszVFAdSki_B2QX8ZQR58uYnXL49XZLEXDBfwK8EKwkA2Qe&components=buttons"></script>
          <style>
             form .error {
                 color: #ff0000;
             }
         </style>
+
+        <script type="text/javascript">
+          $(document).ready(function() {
+            var items = <?php echo json_encode($items); ?>;
+            $("button").click(function() {
+              $("[name=itemname]").val(items[Number($(this).val())-1].name);
+              $("[name=itemprice]").val(items[Number($(this).val())-1].price);
+              $("#item_modal").modal("show");
+            });
+          });
+          paypal.Buttons({
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  desciption: $("[name=itemname]").val(),
+                  amount: {
+                    value: $("[name=itemprice]").val()
+                  }
+                }]
+              });
+            },
+            onApprove: function (data, actions) {
+              return actions.order.capture().then(
+                function (details) {
+                  var msg = 'Transaction comp leted by ' + details.payer.name.given_name + '<br/>';
+                  msg += 'Total: ' + details.purchase_unit[0].amount.value;
+                  $('result').html(msg)
+                });
+            }
+          }).render('#paypal-button-container');
+        </script>
     </head>
     <body>
         <div class="container">
@@ -45,8 +77,8 @@ mysqli_close($link);
                             </div>
                         </div>
                     </div>
-                <?php } ?> 
-            </div> 
+                <?php } ?>
+            </div>
         </div>
 
         <!-- Bootstrap modal -->
@@ -61,7 +93,7 @@ mysqli_close($link);
                         <div class="form-body">
                             <div class="form-group">
                                 <label>Item Name</label>
-                                <input name="itemname" class="form-control" type="text" readonly>                                   
+                                <input name="itemname" class="form-control" type="text" readonly>
                             </div>
                             <div class="form-group">
                                 <label>Item Price</label>
@@ -79,6 +111,8 @@ mysqli_close($link);
                             </div>
                         </div>
                         <div class="modal-footer">
+                          <div id="paypal-button-container"></div>
+                          <div id="result"></div>
                         </div>
                     </div><!-- /.modal-body -->
                 </div><!-- /.modal-content -->
